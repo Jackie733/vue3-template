cbebe6d71ccb463235da655e9cd5796773f23fe5
Jackie Tsien <jackie10673311@gmail.com>
2023-12-06 15:59:14 +0800

    update files

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src")
+      '@': path.resolve(__dirname, 'src')
     }
   },
   server: {
     port: 8080,
     proxy: {
-      "/api": {
-        target: "your https address",
+      '/api': {
+        target: 'your https address',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "")
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
